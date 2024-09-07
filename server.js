568c2b754e0338abe9c2570a8103065c64b6a037
Carolyn Wilson <carolynw0902@gmail.com>
2024-09-07 13:52:13 -0400

    created index.ejs

---
+++ server.js
@@ -0,0 +1,19 @@
+//Declare Variables
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const nodemon = require('nodemon')
+
+//Import function
+
+
+//Connect to databse
+
+
+//set middleware
+
+
+//set routes
+
+
+//start server
